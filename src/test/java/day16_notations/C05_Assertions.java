e96e71a237215df7d0fc67fca86e909a9f6026d8
murat akdeniz <akdenizmurat5205@gmail.com>
2022-05-27 13:45:56 +0300

    27.05.2022

--- src/test/java/day16_notations/C05_Assertions.java
+++ src/test/java/day16_notations/C05_Assertions.java
@@ -1,17 +1,32 @@
 package day16_notations;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import utilities.TestBase;
 
 public class C05_Assertions extends TestBase {
-    //1-amazon ana sayfaya gidin
+
+
+    @Test
+    public void test01() {
+        //1-amazon ana sayfaya gidin
+driver.get("https://amazon.com");
 //2-title in Amazon icerdigini test edin
+        Assert.assertTrue(driver.getTitle().contains("Amazon"));
 //3-arama kutusnun erisilebilir oldugunu test edin
+       WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        Assert.assertTrue(aramaKutusu.isEnabled());
 //4-arama kutusuna Nutella yazıp aratın
-//5-arama yapıldıgını test edin
-//6-arama sonucunun Nutella icerdigini test edin
+        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+        // 5- arama yapildigini test edin
+        WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(sonucYaziElementi.isDisplayed());
+        // 6- arama sonucunun Nutella icerdigini test edin
+        Assert.assertTrue(sonucYaziElementi.getText().contains("Kutella"));
 
 
-
-
-
+    }
 }
